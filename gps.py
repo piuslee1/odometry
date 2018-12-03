19999cc95d2ce58a9db558fc78e1e8abf4a5cf4b
lee-aaron <lee.aaron.68@gmail.com>
2018-12-02 16:30:24 -0800

    Updates

--- gps.py
+++ gps.py
@@ -4,7 +4,22 @@ import pynmea2
 import redis
 from time import sleep
 
-port = "/dev/ttyS0"
+port = "/dev/ttyACM0"
+
+def main_arduino():
+	print("Running GPS Test Arduino")
+	ser = serial.Serial(port, 9600)
+	while True:
+		try:
+			gpsdat = ser.readline()
+			print(ser.read(1))
+			if gpsdat[0:6] == "$GPGGA":
+				msg = pynmea2.parse(gpsdat)
+				print("Lat: ",  msg.latitude)
+				print("Long: ", msg.longitude)
+		except Exception as e:
+			print(e)
+			return
 
 def main():
 	print("Running GPS Test")
@@ -21,4 +36,4 @@ def main():
 			return
 
 if __name__ == '__main__':
-	main()
+	main_arduino()
