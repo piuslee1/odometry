ff85f33f63463b2378caa5eb5c0218307a7fbc21
lee-aaron <lee.aaron.68@gmail.com>
2019-01-27 11:50:38 -0800

    Fix formatting

--- gps.py
+++ gps.py
@@ -7,35 +7,39 @@ from time import sleep
 # Change port according to setup
 port = "/dev/ttyS1"
 
+
 def main_arduino():
-	print("Running GPS Test Arduino")
-	ser = serial.Serial(port, 9600)
-	while True:
-		try:
-			gpsdat = ser.readline()
-			if gpsdat[0:6] == "$GPGGA":
-				msg = pynmea2.parse(gpsdat)
-				print "Lat: ",  msg.latitude
-				print "Long: ", msg.longitude
-		except Exception as e:
-			print e
-			return
+    print("Running GPS Test Arduino")
+    ser = serial.Serial(port, 9600)
+    while True:
+        try:
+            gpsdat = ser.readline()
+            if gpsdat[0:6] == "$GPGGA":
+                msg = pynmea2.parse(gpsdat)
+                print "Lat: ",  msg.latitude
+                print "Long: ", msg.longitude
+        except Exception as e:
+            print e
+            return
+
 
 def main():
-	print("Running GPS Test")
-	ser = serial.Serial(port, 9600)
-	r = redis.Redis(host='192.168.7.1', port='6379')
-	p = r.pubsub()
-	while True:
-		try:
-			gpsdat = ser.readline()
-			if gpsdat[0:6] == "$GPGGA":
-				msg = pynmea2.parse(gpsdat)
-        text = '(' + str(msg.latitude) + str(msg.lat_dir) + ',' + str(msg.longitude) + str(msg.lon_dir) + ')'
-        r.publish('SENSORS:GPS', text)
-		except Exception as e:
-			print(e)
-			return
+    print("Running GPS Test")
+    ser = serial.Serial(port, 9600)
+    r = redis.Redis(host='192.168.7.1', port='6379')
+    p = r.pubsub()
+    while True:
+        try:
+            gpsdat = ser.readline()
+            if gpsdat[0:6] == "$GPGGA":
+                msg = pynmea2.parse(gpsdat)
+                text = '(' + str(msg.latitude) + str(msg.lat_dir) + \
+                    ',' + str(msg.longitude) + str(msg.lon_dir) + ')'
+                r.publish('SENSORS:GPS', text)
+        except Exception as e:
+            print(e)
+            return
+
 
 if __name__ == '__main__':
-	main()
+    main()
